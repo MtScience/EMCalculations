98be720ae7837abd41e8aefb360f19bdce0f69bb
MtScience <107070506+MtScience@users.noreply.github.com>
2022-11-18 11:15:17 +0300

    File that contains some needed material constants

---
+++ common/constants.py
@@ -0,0 +1,21 @@
+"""
+Модуль, содержащий физические константы, используемые в расчётах.
+
+Постоянные:
+
+* ``COPPER_CONDUCTIVITY: float``
+
+  Проводимость меди, 1/Ом∙мм
+
+* ``COPPER_DENSITY: float``
+
+  Плотность меди, кг/м³
+
+* ``STEEL_DENSITY: float``
+
+  Плотность электротехнической стали, кг/м³
+"""
+
+COPPER_CONDUCTIVITY: float = 57000
+COPPER_DENSITY: float = 8920
+STEEL_DENSITY: float = 7600
